b391cc7f0a1aca3db9af4681a9b46348a7d6bf6a
ZZ1975 <3402468579@qq.com>
2025-02-07 07:45:19 +0800

    New version about collisions plus(Zhi Zhao)

--- iron_rebellion/chapter3Story.js
+++ iron_rebellion/chapter3Story.js
@@ -103,94 +103,87 @@ class Chapter3Story {
     }
   }
 
+  // New version about collisions(Zhi Zhao)
   collisionHandle() {
-    // 处理robotDog和enemyDog的碰撞
+    // Handle collisions between robotDog and enemyDogs
     for (let i = this.enemyDogs.length - 1; i >= 0; i--) {
       let enemyDog = this.enemyDogs[i];
       if (this.collisionCheck(this.robotDog, enemyDog)) {
-        this.hud.removeLife();
-        // 使用 splice 方法删除特定的 enemyDog 对象
+        this.hud.lives -= 1;
         this.enemyDogs.splice(i, 1);
-        this.robotDog.x = 50;
-        this.robotDog.y = 50;
       }
     }
 
-    // 处理robotDog和drone的碰撞
-    for (let i = this.drones.length - 1; i >= 0; i--) {
-      let drone = this.drones[i];
-      if (this.collisionCheck(this.robotDog, drone)) {
-        this.hud.removeLife();
-        // 使用 splice 方法删除特定的 drone 对象
-        this.drones.splice(i, 1);
-      }
-    }
-
-    // 处理robotDog和platform的碰撞
+    // Handle collisions between robotDog and platforms
     this.robotDog.onGround = false;
-    for (let i = this.platforms.length - 1; i >= 0; i--) {
-      let platform = this.platforms[i];
-      if (this.collisionCheck(this.robotDog, platform) &&
-        !(keyIsDown(87) || keyIsDown(119))) {
+    for (let platform of this.platforms) {
+      if (!this.collisionCheck(this.robotDog, platform)) continue;
+
+      let prevX = this.robotDog.x - this.robotDog.velocityX;
+      let prevY = this.robotDog.y - this.robotDog.velocityY;
+      let wasAbove = prevY + this.robotDog.height / 2 <= platform.y - platform.height / 2;
+      let wasBelow = prevY - this.robotDog.height / 2 >= platform.y + platform.height / 2;
+      let wasLeft = prevX + this.robotDog.width / 2 <= platform.x - platform.width / 2;
+      let wasRight = prevX - this.robotDog.width / 2 >= platform.x + platform.width / 2;
+
+      if (wasAbove && this.robotDog.velocityY > 0) {
+        this.robotDog.y = platform.y - platform.height / 2 - this.robotDog.height / 2;
+        this.robotDog.velocityY = 0;
         this.robotDog.onGround = true;
-      } else if (this.robotDog.velocityY > 0 && this.collisionCheck(this.robotDog, platform)) {
-        // 正在下落且碰撞到平台则说明已经onGround
-        this.robotDog.isJumping = false;
-        this.robotDog.onGround = true;
-      } 
+      } else if (wasBelow && this.robotDog.velocityY < 0) {
+        this.robotDog.y = platform.y + platform.height / 2 + this.robotDog.height / 2;
+        this.robotDog.velocityY = 0;
+      }
+
+      if (wasLeft && this.robotDog.velocityX > 0) {
+        this.robotDog.x = platform.x - platform.width / 2 - this.robotDog.width / 2;
+        this.robotDog.velocityX = 0;
+      } else if (wasRight && this.robotDog.velocityX < 0) {
+        this.robotDog.x = platform.x + platform.width / 2 + this.robotDog.width / 2;
+        this.robotDog.velocityX = 0;
+      }
     }
-    // 处理enemyDog和platform的碰撞
-    for (let i = this.enemyDogs.length - 1; i >= 0; i--) {
-      let enemyDog = this.enemyDogs[i];
+
+    // Handle collisions between enemyDogs and platforms
+    for (let enemyDog of this.enemyDogs) {
       enemyDog.onGround = false;
-      for (let j = this.platforms.length - 1; j >= 0; j--) {
-        let platform = this.platforms[j];
-        if (this.collisionCheck(enemyDog, platform)) {
+      for (let platform of this.platforms) {
+        if (this.collisionCheck(enemyDog, platform) && enemyDog.velocityY >= 0) {
           enemyDog.onGround = true;
+          enemyDog.velocityY = 0;
+          enemyDog.y = platform.y - platform.height / 2 - enemyDog.height / 2;
+          break;
         }
       }
     }
-    // 处理battery和platform的碰撞
-    for (let i = this.batteries.length - 1; i >= 0; i--) {
-      let battery = this.batteries[i];
+
+    // Handle collisions between batteries and platforms
+    for (let battery of this.batteries) {
       battery.onGround = false;
-      for (let j = this.platforms.length - 1; j >= 0; j--) {
-        let platform = this.platforms[j];
+      for (let platform of this.platforms) {
         if (this.collisionCheck(battery, platform)) {
           battery.onGround = true;
+          break;
         }
       }
     }
-    // 处理robotDog和battery的碰撞
+
+    // Handle collisions between robotDog and batteries
     for (let i = this.batteries.length - 1; i >= 0; i--) {
       let battery = this.batteries[i];
       if (this.collisionCheck(this.robotDog, battery)) {
-        this.hud.addLife();
-        // 使用 splice 方法删除特定的 battery 对象
+        this.hud.lives += 1;
         this.batteries.splice(i, 1);
       }
     }
   }
 
-
-  collisionCheck(object1, object2) {
-    // 检查 object1 是否在 object2 的左侧
-    if (object1.x + object1.width/2.0 < object2.x - object2.width/2.0) {
-      return false;
-    }
-    // 检查 object1 是否在 object2 的右侧
-    if (object1.x - object1.width/2.0 > object2.x + object2.width/2.0) {
-      return false;
-    }
-    // 检查 object1 是否在 object2 的上方
-    if (object1.y + object1.height/2.0 < object2.y - object2.height/2.0) {
-      return false;
-    }
-    // 检查 object1 是否在 object2 的下方
-    if (object1.y - object1.height/2.0 > object2.y + object2.height/2.0) {
-      return false;
-    }
-    return true;
+  collisionCheck(a, b) {
+    return !(
+      a.x + a.width / 2 <= b.x - b.width / 2 ||
+      a.x - a.width / 2 >= b.x + b.width / 2 ||
+      a.y + a.height / 2 <= b.y - b.height / 2 ||
+      a.y - a.height / 2 >= b.y + b.height / 2
+    );
   }
-
 }
